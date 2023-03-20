e49ed9979a51a47f4d1088f8fe9ee0ddf4dbc3f0
Eliott Caen <eliott.caen@outlook.com>
2023-03-20 20:34:11 +0200

    commit before merging

--- get_all_hike_info.py
+++ get_all_hike_info.py
@@ -13,7 +13,7 @@ logging.basicConfig( format='%(asctime)s ---- %(levelname)s:%(message)s - row %(
 
 def main():
     base_url = "https://www.komoot.com/discover/Lyon/@45.7575926%2C4.8323239/tours?max_distance=200000&sport=hike&map=true&pageNumber="  # Url for 5km Lyon
-    amount_of_pages_to_be_scraped = 200
+    amount_of_pages_to_be_scraped = 2
     list_of_hiking_urls = GU.get_all_hiking_urls(base_url, amount_of_pages_to_be_scraped)
     print("list of hiking urls done")
     hike_ID = 1
