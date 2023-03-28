02d00251e848942c97eb0bf688d632fa7891b394
Eliott Caen <eliott.caen@outlook.com>
2023-03-28 17:22:23 +0300

    eliott test

--- get_all_hike_info.py
+++ get_all_hike_info.py
@@ -13,7 +13,7 @@ logging.basicConfig( format='%(asctime)s ---- %(levelname)s:%(message)s - row %(
 
 def main():
     base_url = "https://www.komoot.com/discover/Lyon/@45.7575926%2C4.8323239/tours?max_distance=200000&sport=hike&map=true&pageNumber="  # Url for 5km Lyon
-    amount_of_pages_to_be_scraped = 3
+    amount_of_pages_to_be_scraped = 200
     use_csv = input("""Would you like to use the links stored in the list_of_hiking_urls.csv file, then type "Y".
      If you would like to determine the list of urls by running the code then type "N".
      Answer: """)
