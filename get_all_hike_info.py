ffe6d3c609ff957d15b2efafd445da7e9cb47bf6
Eliott Caen <eliott.caen@outlook.com>
2023-03-30 19:47:12 +0300

    split the functions in smaller functions

--- get_all_hike_info.py
+++ get_all_hike_info.py
@@ -1,64 +1,79 @@
-import get_hiking_urls as GU
-import get_hike_info as HI
-from selenium import common
+import get_hiking_urls as gu
+import get_hike_info as hi
 from tqdm.auto import tqdm
 import csv
+import json
 import logging
 
-logging.basicConfig( format='%(asctime)s ---- %(levelname)s:%(message)s - row %(lineno)d',
+logging.basicConfig(format='%(asctime)s ---- %(levelname)s:%(message)s - row %(lineno)d',
                     level=logging.INFO)
-# filename='hike_scrapping.log',
+
+with open("komoot_config.json", 'r') as f:
+    config = json.load(f)
+NUMBER_OF_PAGES_TO_SCRAP = config["NUMBER_OF_PAGES_TO_SCRAP"]
+BASE_URL = config["URL"]
+HIKING_DATA_CSV = config["HIKES_INFO_CSV"]
 
 
-
-def main():
-    base_url = "https://www.komoot.com/discover/Lyon/@45.7575926%2C4.8323239/tours?max_distance=200000&sport=hike&map=true&pageNumber="  # Url for 5km Lyon
-    amount_of_pages_to_be_scraped = 200
+def ask_user_choice():
+    """
+    Ask the user whether he wants to re-use the previous list of hike urls (choice Y) or re-run the whole code,
+    including the collection of hike urls (choice N)
+    """
     use_csv = input("""Would you like to use the links stored in the list_of_hiking_urls.csv file, then type "Y".
-     If you would like to determine the list of urls by running the code then type "N".
-     Answer: """)
+         If you would like to determine the list of urls by running the code then type "N".
+         Answer: """)
     if use_csv == "N":
-        list_of_hiking_urls = GU.get_all_hiking_urls(base_url, amount_of_pages_to_be_scraped)
-        GU.write_urls_to_csv(list_of_hiking_urls)
+        hiking_urls = gu.get_all_hikes_urls(BASE_URL, NUMBER_OF_PAGES_TO_SCRAP)
+        gu.write_urls_to_csv(hiking_urls)
     elif use_csv == "Y":
         with open("list_of_hiking_urls.csv", "r") as hike_urls_csv:
-            list_of_hiking_urls = hike_urls_csv.read().splitlines()
+            hiking_urls = hike_urls_csv.read().splitlines()
     else:
         print("Please run code again and give correct input this time")
     print("list of hiking urls done")
-    hike_ID = 1
-    list_of_hikes = []
+    return hiking_urls
 
-    for url in tqdm(list_of_hiking_urls):  # check if iterator is well set, for testing put [:2]
-        print(url)
-        # For each hike, we try to retrieve all the hike infos and put the resulting dictionary in a list
-        try:
-            hike = {}
-            drive = HI.driver_get_url(url)
-            hike["1.ID"] = hike_ID
-            hike.update(HI.get_hike_info(drive))
-            hike_ID += 1
-            list_of_hikes.append(hike)
-            print(hike)
-        # In case one of the info of the hike was not retrieved, we don't record this hike at all
-        except:
-            logging.warning(f'The hike of the {url} url was not recorded')
 
+def list_of_keys(hikes_infos):
+    """
+    This function takes a list of dictionaries, and return the keys across all dictionaries
+    This will be used to create the column of our csv
+    In our code, each dictionary stores the info of one hike, and so the list of dictionaries store the info of all the hikes
+    This step is necessary because all the hikes do not have the same info on the website, and therefore we do not have the same keys for each dictionary
+    """
     set_of_all_keys = set()
-    for hike in list_of_hikes:
+    for hike in hikes_infos:
         set_of_all_keys.update(list(hike.keys()))
-    list_of_keys = (list(set_of_all_keys))
-    list_of_keys.sort()
+    return sorted(list(set_of_all_keys))
 
-    csv_colums = list_of_keys
-    print(list_of_keys)
 
-    csv_file = "Hiking_data.csv"
-    with open(csv_file, 'w', newline='') as csv_file:
+def write_csv(hikes_infos):
+    """
+    Write the csv from the list of hikes infos
+    """
+    csv_colums = list_of_keys(hikes_infos)
+    with open(HIKING_DATA_CSV, 'w', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=csv_colums)
         writer.writeheader()
-        for data in list_of_hikes:
+        for data in hikes_infos:
             writer.writerow(data)
+
+
+def main():
+    # Ask the choice of the user: re-use the last saved list of hike urls, or collect them again from a catalogue page
+    hiking_urls = ask_user_choice()
+    # Create the list that will store all the hikes infos (the infos of one hike are stored in a dictionary)
+    hikes_infos = []
+
+    # For each hike url, we call get_hike_info(). Once we collected the info (and stored it in a dictionary),
+    # we store it in the list 'hikes_infos'
+    for hike_id, hike_url in enumerate(tqdm(hiking_urls)):
+        hikes_infos.append(hi.get_hike_info(hike_id, hike_url))
+
+    # We create a csv from the list of hikes infos
+    write_csv(hikes_infos)
+
 
 if __name__ == "__main__":
     main()
--- get_hiking_urls.py
+++ get_hiking_urls.py
@@ -18,47 +18,68 @@ driver = Chrome(options=options, service=chrome_service)
 driver.implicitly_wait(5)
 
 
-def get_all_hiking_urls(base_url, amount_of_pages_to_be_scraped):
+def get_all_catalogues_urls(base_url, number_of_page_to_scrap):
     """
-    :param  - url of the website containing the urls to the hikes that are to be scraped
-            - amount of sub-webpages to be scraped (beware that the num of pages to be scraped must be less or equal
-            than the available amount of webpages.
-    :return: a list of urls each corresponding to a specific hike to be scraped
+    The hikes are displayed by set of 12. Each set of 12 hikes is on a different "catalogue page".
+    This function collect all the catalogue urls from the base page.
     """
-    list_of_urls = []
+    catalogues_urls = list()
+    for page_num in range(1, number_of_page_to_scrap + 1):
+        catalogues_urls.append(f"{base_url}" + str(page_num))
+    return catalogues_urls
+
+
+def get_html_blocks(catalogue_url):
+    """
+    On one catalogue page, get the 12 html blocks that contains the 12 hikes url
+    """
+    driver.get(catalogue_url)
+    time.sleep(10)
+    try:
+        return driver.find_elements(By.CSS_SELECTOR, "div[class*='css-1dzdr7g']")  # this is a "block"
+    except common.exceptions.WebDriverException as err:
+        logging.error(f'the contents of this url {catalogue_url} was not found: {err}')
+
+
+def get_one_hike_url(block, urls_of_hikes, number_of_hikes_found_on_this_page, number_of_hikes_found, catalogue_url):
+    """
+
+    :param block:
+    :param urls_of_hikes:
+    :param number_of_hikes_found_on_this_page:
+    :param number_of_hikes_found:
+    :param catalogue_url:
+    :return:
+    """
+    try:
+        urls_of_hikes.append(block.find_element(By.TAG_NAME, "a").get_attribute("href"))
+        number_of_hikes_found_on_this_page += 1
+        number_of_hikes_found += 1
+        logging.info(
+            f'{number_of_hikes_found_on_this_page} hikes url found on this page, {number_of_hikes_found} in total')
+    except common.exceptions.WebDriverException as err:
+        logging.warning(f'The url of one hike on page ({catalogue_url}) was not found: {err}')
+    return (number_of_hikes_found_on_this_page, number_of_hikes_found)
+
+def get_all_hikes_urls(base_url, number_of_pages_to_scrap):
+    """
+    This function puts in music the above functions and get the url within each block
+    From a base url and a number of pages to scrap, it will return the list of hike url
+    """
+    catalogues_urls = get_all_catalogues_urls(base_url, number_of_pages_to_scrap)
     urls_of_hikes = []
-
-    # The hikes are displayed by set of 12. Each set of 12 hikes is on a different page.
-    # In this first step, we collect the url of all the main pages.
-    for page_num in range(1, amount_of_pages_to_be_scraped + 1):
-        list_of_urls.append(f"{base_url}" + str(page_num))
-
-    # The step 2 is to go through each one of these main pages, and collect the url of each of the hike
     number_of_hikes_found = 0
-    for url in tqdm(list_of_urls):
-        try:
-            driver.get(url)
-            time.sleep(2)
-        except common.exceptions.WebDriverException as err:
-            logging.error(f'url {url} was not reached: {err}')
-            continue
 
-        try:
-            contents = driver.find_elements(By.CSS_SELECTOR, "div[class*='css-1dzdr7g']")
-        except common.exceptions.WebDriverException as err:
-            logging.error(f'the contents of url {url} was not found: {err}')
-            continue
+    for catalogue_url in tqdm(catalogues_urls):
+        blocks = get_html_blocks(catalogue_url)
 
         number_of_hikes_found_on_this_page = 0
-        for content in contents:
-            try:
-                urls_of_hikes.append(content.find_element(By.TAG_NAME, "a").get_attribute("href"))
-                number_of_hikes_found_on_this_page += 1
-                number_of_hikes_found += 1
-                logging.info(
-                    f'{number_of_hikes_found_on_this_page} hikes url found on this page, {number_of_hikes_found} in total')
-            except common.exceptions.WebDriverException as err:
-                logging.warning(f'The url of one hike on page ({url}) was not found: {err}')
+        for block in blocks:
+            result = get_one_hike_url(block, urls_of_hikes, number_of_hikes_found_on_this_page, number_of_hikes_found,
+                             catalogue_url)
+            number_of_hikes_found_on_this_page = result[0]
+            number_of_hikes_found = result[1]
+
     return urls_of_hikes
 
 
